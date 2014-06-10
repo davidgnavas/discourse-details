b25f40940367aee37a004196e2b4b436aea3ab5f
NomNuggetNom <NomNuggetNom@yahoo.com>
2014-06-10 18:23:43 -0400

    Create details.js

---
+++ assets/javascripts/details.js
@@ -0,0 +1,16 @@
+function details(text) {
+  return text.replace(\[detail\], "<details>")
+             .replace(\[summary\], "<summary>")
+             .replace(\[\/summary\], "<summary>")
+-             .replace(\[\/detail\], "</details>")
+}
+
+Discourse.Dialect.postProcessText(function (text) {
+  text = [].concat(text);
+  for (var i = 0; i < text.length; i++) {
+    if (text[i].length > 0 && text[i][0] !== "<") {
+      text[i] = details(text[i]);
+    }
+  }
+  return text;
+});
