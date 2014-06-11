dc53e8abdcf4c00de69ddd43bcd3f3f6944b089a
NomNuggetNom <NomNuggetNom@yahoo.com>
2014-06-10 20:12:33 -0400

    Update details.js

--- assets/javascripts/details.js
+++ assets/javascripts/details.js
@@ -2,15 +2,15 @@ function details(text) {
   return text.replace("[detail]", "<details>")
              .replace("[summary]", "<summary>")
              .replace("[/summary]", "</summary>")
-             .replace("[/detail]", "</details>")
+             .replace("[/detail]", "</details>");
 }
 
 Discourse.Dialect.postProcessText(function (text) {
   text = [].concat(text);
   for (var i = 0; i < text.length; i++) {
-    if (text[i].length > 0 && text[i][0] !== "<") {
+    //if (text[i].length > 0 && text[i][0] !== "<") {
       text[i] = details(text[i]);
-    }
+    //}
   }
   return text;
 });
