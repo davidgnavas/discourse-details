90919c7b44fab01b54eb09ec1f85e43a21f08f3d
NomNuggetNom <NomNuggetNom@yahoo.com>
2014-06-11 19:22:26 -0400

    Re-did detection

--- assets/javascripts/details.js
+++ assets/javascripts/details.js
@@ -1,8 +1,17 @@
 function details(text) {
+  var adjusted = "<details><summary>"
+    + srctext.replace(/.*\[details "(.*)"]\s*(.*)\s*\[\/details]/, "$1")
+    + "</summary>"
+    + srctext.replace(/.*\[details "(.*)"]\s*(.*)\s*\[\/details]/, "$2")
+    + "</details>";
+
+ return adjusted;
+  /*
   return text.replace("[detail]", "<details>")
              .replace("[summary]", "<summary>")
              .replace("[/summary]", "</summary>")
              .replace("[/detail]", "</details>");
+             */
 }
 
 Discourse.Dialect.postProcessText(function (text) {
