c601cbbdff3655f3ab534c286fd6495336bf60c1
NomNuggetNom <NomNuggetNom@yahoo.com>
2014-06-11 17:17:50 -0400

    Update details.js

--- assets/javascripts/details.js
+++ assets/javascripts/details.js
@@ -8,9 +8,9 @@ function details(text) {
 Discourse.Dialect.postProcessText(function (text) {
   text = [].concat(text);
   for (var i = 0; i < text.length; i++) {
-    //if (text[i].length > 0 && text[i][0] !== "<") {
+    if (text[i].length > 0 && text[i][0] !== "<") {
       text[i] = details(text[i]);
-    //}
+    }
   }
   return text;
 });
