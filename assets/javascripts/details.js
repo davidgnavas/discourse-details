28e04e3062b4d838186102e436e9532ca790f7f4
NomNuggetNom <nomnuggetnom@gmail.com>
2014-06-22 15:53:43 -0400

    Update details.js

--- assets/javascripts/details.js
+++ assets/javascripts/details.js
@@ -41,6 +41,6 @@ Discourse.Dialect.replaceBlock({
   rawContents: true,
 
   emitter: function(blockContents) {
-    return ['details', blockContents.join("\n"))];
+    return ['details', blockContents.join("\n")];
   }
 });
