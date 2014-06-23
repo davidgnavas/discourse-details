00b693e76a8d753461faf461e58465c3d123919a
NomNuggetNom <nomnuggetnom@gmail.com>
2014-06-23 13:13:59 -0400

    Delete summary.js

--- assets/javascripts/summary.js
+++
@@ -1,8 +0,0 @@
-Discourse.Dialect.replaceBlock({
-  start: /(\[summary\])([\s\S]*)/igm,
-  stop: '[/summary]',
-
-  emitter: function(blockContents) {
-    return ['summary'].concat(this.processInline(blockContents));
-  }
-});
