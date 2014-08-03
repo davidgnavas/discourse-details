7da53ec3a54a19f77231f0a246931bfbc6749b55
NomNuggetNom <nomnuggetnom@gmail.com>
2014-08-03 10:02:33 -0400

    Update details.js

--- assets/javascripts/details.js
+++ assets/javascripts/details.js
@@ -1,43 +1,13 @@
-/*
-function details(text) {
-  //Expression to find.
-  var re = /\[details "(.*)"]\s*(.*)\s*\[\/details]/;
-  // Adjust text.
-  var adjusted = text.replace(re, "<details><summary>" + "$1" + "</summary>" + "$2" + "</details>");
+(function () {
+  
+    Discourse.Dialect.inlineBetween({
+        start: "[hide=",
+        stop:  "[/hide]",
+        rawContents: true,
+        emitter: function(contents) {
+            var matches = contents.match(/(.+)]([\s\S]*)/);
+            if (matches) return ['details', ['summary', ['b', matches[1]]]].concat(this.processInline(matches[2]));
+        }
+    });
 
-  return adjusted;
-}
-*/
-
-/*
-Discourse.Dialect.postProcessText(function (text) {
-  var textBlock = "";
-  text = [].concat(text);
-  for (var i = 0; i < text.length; i++) {
-    if (text[i].length > 0 && text[i][0] !== "<") {
-      textBlock = textBlock + text[i];
-      //text[i] = details(text[i]);
-    }
-  }
-  replaceDetails(textBlock);
-  return text;
-});
-*/
-
-Discourse.Dialect.replaceBlock({
-  start: /(\[details\])([\s\S]*)/igm,
-  stop: '[/details]',
-
-  emitter: function(blockContents) {
-    return ['details'].concat(this.processInline(blockContents));
-  }
-});
-
-Discourse.Dialect.replaceBlock({
-  start: /(\[summary\])([\s\S]*)/igm,
-  stop: '[/summary]',
-
-  emitter: function(blockContents) {
-    return ['summary'].concat(this.processInline(blockContents));
-  }
-});
+}).call(this);
