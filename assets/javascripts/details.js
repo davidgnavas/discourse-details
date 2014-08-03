5baef7ea8d3a5da2a9e50483124710c3edbcfd60
NomNuggetNom <nomnuggetnom@gmail.com>
2014-08-03 12:11:50 -0400

    Update details.js

--- assets/javascripts/details.js
+++ assets/javascripts/details.js
@@ -6,7 +6,7 @@
         rawContents: true,
         emitter: function(contents) {
             var matches = contents.match(/(.+)]([\s\S]*)/);
-            if (matches) return ['details', ['summary', this.processInline(matches[1])]].concat(this.processInline(matches[2]));
+            if (matches) return ['details', ['summary', ''].concat(this.processInline(matches[1]))].concat(this.processInline(matches[2]));
         }
     });
 
