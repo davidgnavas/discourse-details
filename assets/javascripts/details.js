40deb6760dbb4395a9473e27db41af509424003f
David García-Navas López de Cuéllar <davidgarcianavas@gmail.com>
2014-09-29 17:50:08 +0200

    Update details.js

--- assets/javascripts/details.js
+++ assets/javascripts/details.js
@@ -1,4 +1,4 @@
-var tag = "details";
+var tag = "detalles";
 
 (function () {
   
@@ -8,7 +8,7 @@ var tag = "details";
         rawContents: true,
         emitter: function(contents) {
             var matches = contents.match(/(.+)]([\s\S]*)/);
-            if (matches) return ['details', ['summary', ''].concat(this.processInline(matches[1]))].concat(this.processInline(matches[2]));
+            if (matches) return ['detalles', ['resumen', ''].concat(this.processInline(matches[1]))].concat(this.processInline(matches[2]));
         }
     });
 
